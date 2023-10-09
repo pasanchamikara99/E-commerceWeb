a6831bb5ceaaa92a4a2f863a53988498ae153910
pasanchamikara99 <pasanchamikara989@gmail.com>
2023-10-09 23:48:12 +0530

    again

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import { Link } from "react-router-dom";
 
 export const Profile = () => {
   return (
@@ -12,7 +13,9 @@ export const Profile = () => {
           <label htmlFor="">Pasan</label>
           <label htmlFor="">pasasnchamikara989@gmail.com</label>
 
-          <Link to="/myOrders"></Link>
+          <Link to="/myOrders">My Orders</Link>
+          <Link to="/myOrders">Edit Address</Link>
+          <Link to="/myOrders">Edit Account</Link>
         </div>
       </div>
     </div>
